4d454f56683d959742144748256c54483d34891c
blank1024 <13380497335@163.com>
2022-07-19 21:50:43 +0800

    feat: implement calculateReceipt methods

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,11 +1,13 @@
 package pos.machine;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
         HashMap<String, Integer> barcodesMap = calculateItemNumber(barcodes);
+        Receipt receipt = calculateReceipt(barcodesMap);
         return null;
     }
 
@@ -19,5 +21,30 @@ public class PosMachine {
             }
         }
         return barcodesMap;
+    }
+
+    private Receipt calculateReceipt(HashMap<String, Integer> barcodesMap) {
+        List<ItemInfo> itemList = ItemDataLoader.loadAllItemInfos();
+        List<ReceiptItem> receiptList = calculateReceiptItems(itemList, barcodesMap);
+        int totalPrice = calculateTotalPrice(receiptList);
+        return new Receipt(receiptList, totalPrice);
+    }
+
+    private List<ReceiptItem> calculateReceiptItems(List<ItemInfo> itemList, HashMap<String, Integer> barcodesMap) {
+        List<ReceiptItem> receiptItems = new ArrayList<>();
+        for (ItemInfo itemInfo : itemList) {
+            ReceiptItem receiptItem = new ReceiptItem(itemInfo.getName(), barcodesMap.get(itemInfo.getBarcode()),
+                    itemInfo.getPrice(), itemInfo.getPrice() * barcodesMap.get(itemInfo.getBarcode()));
+            receiptItems.add(receiptItem);
+        }
+        return receiptItems;
+    }
+
+    private int calculateTotalPrice(List<ReceiptItem> receiptList) {
+        int total = 0;
+        for (ReceiptItem receiptItem : receiptList) {
+            total += receiptItem.getSubTotal();
+        }
+        return total;
     }
 }
